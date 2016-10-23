6f607ef2ed8481a4aab92611334369cc6d61f963
Jakob Ferdinandsen <jakob@jhaaf.dk>
2016-10-23 17:31:39 +0200

    Added sample print to Dummy receiver

--- ControllerConnector.py
+++ ControllerConnector.py
@@ -29,18 +29,19 @@ while True:
             l2 = (j.get_axis(12) + 1) * 128  # 0-256 range
             r2 = (j.get_axis(13) + 1) * 128  # 0-256 range
 
-            if l2 == -1.0 and not (l2Calibrated):
+            if l2 == 0 and not (l2Calibrated):
                 l2Calibrated = True
-            if r2 == -1.0 and not (r2Calibrated):
+            if r2 == 0 and not (r2Calibrated):
                 r2Calibrated = True
 
             if r2Calibrated and l2Calibrated and not (calibrated):
                 calibrated = True
+                print "Calibration complete"
 
             dataTranslator = dataformatter.JoystickDataPacketTranslator()
-
             dataTranslator.interpretDataString(dataTranslator.createDataString(l3x, l3y, r3x, r3y, l2, r2))
-
             dataString = dataTranslator.createDataString(l3x, l3y, r3x, r3y, l2, r2)
-            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            sock.sendto(dataString, (UDP_IP, UDP_PORT))
+
+            if calibrated:
+                sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                sock.sendto(dataString, (UDP_IP, UDP_PORT))
--- DummyDataReceiver.py
+++ DummyDataReceiver.py
@@ -6,8 +6,8 @@ UDP_PORT = int(raw_input("PORT: "))
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
-
 formatter = dataformatter.JoystickDataPacketTranslator()
 while True:
     data, addr = sock.recvfrom(1024)
     formatter.interpretDataString(data)
+    print formatter.l2
